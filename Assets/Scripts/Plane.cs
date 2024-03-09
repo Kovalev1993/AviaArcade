9c9da8cd9dcd612c3975fb8dcc5f94aa0231aa37
Kovalev1993 <Air1618@gmail.com>
2024-03-09 17:25:29 +0300

    Plane prefab. Propeller rotation.

--- Assets/Scripts/Plane.cs
+++ Assets/Scripts/Plane.cs
@@ -1,16 +1,25 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Plane : MonoBehaviour
 {
-    [SerializeField] private float _speed;
+    [SerializeField] private float _movementSpeed;
+    [SerializeField] private Transform _propeller;
+    [SerializeField] private Vector3 _propellerRotationSpeed;
 
     public float GetSpeed()
     {
-        return _speed;
+        return _movementSpeed;
+    }
+
+    private void Start()
+    {
+        _propeller.Rotate(Random.value * 360 * _propellerRotationSpeed);
     }
 
     private void FixedUpdate()
     {
-        transform.Translate(_speed * transform.forward);
+        transform.Translate(_movementSpeed * transform.forward);
+        _propeller.Rotate(_propellerRotationSpeed);
     }
 }
